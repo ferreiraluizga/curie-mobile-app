2b147af7acff4ebf2a687075da44f656900343a8
RebecadeMouraMendes <rebeca.mendes3@etec.sp.gov.br>
2025-09-01 10:59:35 -0300

    feat: API: ajustes nas tarefas e metas

--- src/main/java/com/curie/curieapp/repository/MetaRepository.java
+++ src/main/java/com/curie/curieapp/repository/MetaRepository.java
@@ -1,9 +1,37 @@
 package com.curie.curieapp.repository;
 
 import com.curie.curieapp.entities.Meta;
+import com.curie.curieapp.entities.enums.Prioridade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MetaRepository extends JpaRepository<Meta, Long> {
+
+    @Query("SELECT * FROM metas WHERE status = 'pendente' AND user_id = :userId")
+    List<Meta> buscarMetasPorUsuario(
+            @Param("userId") Long userId
+    );
+
+    @Query("SELECT * FROM metas WHERE nome = :nome AND user_id = :userId")
+    List<Meta> buscarMetasPorNome(
+            @Param("nome") String nome,
+            @Param("userId") Long userId
+    );
+
+    @Query("SELECT * FROM metas WHERE user_id = :userId order by fim desc")
+    List<Meta> buscarMetasPorPrazo(
+            @Param("userId") Long userId
+    );
+
+    @Query("SELECT * FROM metas WHERE prioridade = :prioridade AND user_id = :userId")
+    List<Meta> buscarMetasPorPrioridade(
+            @Param("prioridade") Prioridade prioridade,
+            @Param("userId") Long userId
+    );
+
 }
--- src/main/java/com/curie/curieapp/repository/TarefaRepository.java
+++ src/main/java/com/curie/curieapp/repository/TarefaRepository.java
@@ -1,9 +1,36 @@
 package com.curie.curieapp.repository;
 
+
 import com.curie.curieapp.entities.Tarefa;
+import com.curie.curieapp.entities.enums.Prioridade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
+    @Query("SELECT * FROM tarefas WHERE status = 'pendente' AND user_id = :userId")
+    List<Tarefa> buscarTarefasPorUsuario(
+            @Param("userId") Long userId
+    );
+
+    @Query("SELECT * FROM tarefas WHERE nome = :nome AND user_id = :userId")
+    List<Tarefa> buscarTarefasPorNome(
+            @Param("nome") String nome,
+            @Param("userId") Long userId
+    );
+
+    @Query("SELECT * FROM tarefas WHERE user_id = :userId order by fim desc")
+    List<Tarefa> buscarTarefasPorPrazo(
+            @Param("userId") Long userId
+    );
+
+    @Query("SELECT * FROM tarefas WHERE prioridade = :prioridade AND user_id = :userId")
+    List<Tarefa> buscarTarefasPorPrioridade(
+            @Param("prioridade") Prioridade prioridade,
+            @Param("userId") Long userId
+    );
 }
