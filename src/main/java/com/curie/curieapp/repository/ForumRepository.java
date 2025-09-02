032a3e55b68933651c3ff64d6502493bdd76b572
BeccaDev <rebeca.mendes3@etec.sp.gov.br>
2025-09-02 15:35:34 -0300

    feat: Métodos para o repository

--- src/main/java/com/curie/curieapp/repository/ForumRepository.java
+++ src/main/java/com/curie/curieapp/repository/ForumRepository.java
@@ -1,7 +1,6 @@
 package com.curie.curieapp.repository;
 
 import com.curie.curieapp.entities.Forum;
-import com.curie.curieapp.entities.Tarefa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,12 +11,12 @@ import java.util.List;
 @Repository
 public interface ForumRepository extends JpaRepository<Forum, Long> {
     @Query(value = "SELECT * FROM foruns WHERE nome = :nome", nativeQuery = true)
-    List<Tarefa> buscarForumPorNome(
+    List<Forum> buscarForumPorNome(
             @Param("nome") String nome
     );
 
     @Query(value = "SELECT mensagens.data, mensagens.texto, mensagens.arquivo FROM mensagens INNER JOIN foruns ON mensagens.forum_id = foruns.forum_id WHERE forum_id = :forumId", nativeQuery = true)
-    List<Tarefa> buscarMensagensPorFurum(
+    List<Forum> buscarMensagensPorFurum(
             @Param("forumId") Long forumId
     );
 
