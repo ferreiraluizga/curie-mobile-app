b654b9b343b709fbf783c5d41cf71c87370b980c
ferreiraluizga <luiz.gabriel.lcf@gmail.com>
2025-11-26 12:24:23 -0300

    feat: conectando app a API

--- app/src/main/java/com/curie/curie/data/api/RetrofitClient.kt
+++ app/src/main/java/com/curie/curie/data/api/RetrofitClient.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://10.0.2.2:8080/"
+    private const val BASE_URL = "https://curie-mobile-app.onrender.com/"
 
     fun <T> createService(serviceClass: Class<T>, tokenProvider: () -> String?): T {
         val okHttpClient = OkHttpClient.Builder()
